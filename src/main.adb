cb3aa3c569405c482f19df7fffb3fc37e5be17ab
Ryan Pietras <18U-RPietras@cap.education>
2021-06-22 13:48:35 +0100

    added use statement

--- src/main.adb
+++ src/main.adb
@@ -1,15 +1,18 @@
 with Ada.Text_IO;
+use Ada.Text_IO;
 
 procedure Main is
 begin
 
    while (True) loop
 
-      Ada.Text_IO.Put("> ");
+      Put("$> ");
       declare
-         input : String := Ada.Text_IO.Get_Line;
+         input : String := Get_Line;
       begin
-         Ada.Text_IO.Put_Line (input);
+         -- once you get input, carry on here
+
+         Put_Line (input);
       end;
 
    end loop;
