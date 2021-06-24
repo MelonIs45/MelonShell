5c62ca756ca244f6748ead57c6c47b8e8776b0e8
Ryan Pietras <18U-RPietras@cap.education>
2021-06-24 14:51:54 +0100

    added cd command

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/user"
 	"strings"
 	_ "strings"
 )
 
+var CurDir, _ = os.Getwd()
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
+	fmt.Println(os.Getenv("Path"))
 	var paths []string
 	path, err := os.Getwd()
 	if err != nil {
@@ -27,22 +31,36 @@ func main() {
 	}
 
 	for {
-		fmt.Print("$> ")
+		userName, _ := user.Current()
+		hostName, _ := os.Hostname()
+
+		fmt.Print(userName.Username + "@" + hostName + ": " + CurDir + "\n> ")
 		input, _ := reader.ReadString('\n')
 
 		if input != "" {
-			fmt.Println(0)
-
 			exec := strings.Split(input, " ")
 
-			if strings.HasPrefix(exec[0], "#/") {
+			if strings.HasPrefix(exec[0], "./") {
 				ExecutePathProgram(exec[0])
 			}
-
+			if strings.HasPrefix(exec[0], "cd") {
+				ChangeDirectory(exec[1:])
+			}
 		}
 	}
 }
 
 func ExecutePathProgram(program string) {
-	fmt.Print(program)
+	fmt.Print(strings.Split(program, "./")[1])
+}
+
+func ChangeDirectory(path []string) {
+	splitDir := strings.Split(CurDir, "\\")
+	if strings.HasPrefix(path[0], "../") || strings.HasPrefix(path[0], "..") {
+		CurDir = strings.Join(splitDir[:len(splitDir)-1], "\\")
+	}
+
+	if strings.HasPrefix(path[0], "~") {
+		CurDir, _ = os.Getwd()
+	}
 }
